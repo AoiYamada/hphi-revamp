e6f81fa6c44e0ca12e33f3bcfb3be4841a45f2c2
AoiYamada <19519928+AoiYamada@users.noreply.github.com>
2024-12-23 02:33:10 +0800

    fix: revalidate problem

--- src/collections/Pages/hooks/revalidatePage.ts
+++ src/collections/Pages/hooks/revalidatePage.ts
@@ -14,7 +14,7 @@ export const revalidatePage: CollectionAfterChangeHook<Page> = ({
   }
 
   if (doc._status === 'published') {
-    const path = doc.slug === 'home' ? '/' : `/${encodeURIComponent(doc.slug ?? '')}`
+    const path = doc.slug === 'home' ? '/' : `/${doc.slug}`
 
     payload.logger.info(`Revalidating page at path: ${path}`)
 
@@ -24,8 +24,7 @@ export const revalidatePage: CollectionAfterChangeHook<Page> = ({
 
   // If the page was previously published, we need to revalidate the old path
   if (previousDoc?._status === 'published' && doc._status !== 'published') {
-    const oldPath =
-      previousDoc.slug === 'home' ? '/' : `/${encodeURIComponent(previousDoc.slug ?? '')}`
+    const oldPath = previousDoc.slug === 'home' ? '/' : `/${previousDoc.slug}`
 
     payload.logger.info(`Revalidating old page at path: ${oldPath}`)
 
