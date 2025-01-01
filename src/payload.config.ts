ea244ce7d151656f4bca540b4c73423c67aad099
AoiYamada <19519928+AoiYamada@users.noreply.github.com>
2025-01-02 04:35:47 +0800

    feat: white label meta

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -62,11 +62,25 @@ export default buildConfig({
       ],
     },
     meta: {
+      description:
+        '專業心理治療(香港)中心 HPHI 是一所為普羅市民提供專業心理輔導服務的機構，並提供一系列的催眠師課程，包括NLP、NGH、PBH 等國際認可的心理治療、催眠課程。',
+      title: '專業心理治療及催眠應用（香港）有限公司',
       icons: [
         {
           url: `${getServerSideURL()}/favicon/favicon.ico`,
         },
       ],
+      openGraph: {
+        description:
+          '專業心理治療(香港)中心 HPHI 是一所為普羅市民提供專業心理輔導服務的機構，並提供一系列的催眠師課程，包括NLP、NGH、PBH 等國際認可的心理治療、催眠課程。',
+        images: [
+          {
+            url: `${getServerSideURL()}/hphi-logo-bg.png`,
+          },
+        ],
+        siteName: '專業心理治療及催眠應用（香港）有限公司',
+        title: '專業心理治療及催眠應用（香港）有限公司',
+      },
     },
   },
   // This config helps us configure global or default features that the other editors can inherit
