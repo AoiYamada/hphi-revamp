b9b68d8da0a4e64013e586cfad55a6283ca528db
AoiYamada <19519928+AoiYamada@users.noreply.github.com>
2025-01-02 01:16:13 +0800

    feat: update placeholder img

--- back.sh
+++ back.sh
@@ -1,3 +1,5 @@
 # example: mongodb://usr:pw@mongodb/db-name?authSource=admin
 mongodump --uri="db-url" --out=/mnt/backups
 zip -r ./hphi-revamp.zip ./hphi-revamp
+
+git pull && nvm use && pnpm i && pnpm build && pm2 reload hphi-revamp
