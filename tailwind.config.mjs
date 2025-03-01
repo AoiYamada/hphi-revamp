4f9794dc731f408f5549e090fcf65b19b3fd8098
AoiYamada <19519928+AoiYamada@users.noreply.github.com>
2025-03-02 03:23:00 +0800

    fix: hr color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -126,7 +126,7 @@ const config = {
         DEFAULT: {
           css: [
             {
-              '--tw-prose-hr': 'var(--border)',
+              '--tw-prose-hr': 'hsl(var(--border))',
               // colors are undefined here, so I can control with classes
               '--tw-prose-body': 'var(--text)',
               '--tw-prose-links': 'var(--text)',
