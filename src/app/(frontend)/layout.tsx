2b941a0a45d26d20fdd193248665736e72ef592c
AoiYamada <19519928+AoiYamada@users.noreply.github.com>
2025-05-21 15:07:43 +0800

    feat: gtm code

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -12,6 +12,7 @@ import { Providers } from '@/providers'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import { draftMode } from 'next/headers'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
 
 import './globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
@@ -39,6 +40,14 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {googleSiteVerification && (
           <meta name="google-site-verification" content={googleSiteVerification} />
+        )}
+        {!isDev && (
+          <Script id="gtm-script" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PKF7RMHZ');`}
+          </Script>
         )}
       </head>
       <body className="bg-primary/10">
